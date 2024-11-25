e85cc668e0db7c030b246a2652d0a34adb3600b2
Andrei <thefurdui@gmail.com>
2024-11-25 17:57:36 +0100

    report shows outage duration

--- generate-error-report.sh
+++ generate-error-report.sh
@@ -5,18 +5,30 @@ ERROR_REPORT="network_drops_report.txt"
 
 # Get all lines with DROP and calculate time differences
 gawk '
-    /\| DROP$/ {
-        # Remove "| DROP " from the line
-        line = substr($0, 1, length($0) - 7)
+    {
+        # Store timestamp for all entries (both successful and drops)
         current_time = mktime(gensub(/^([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2}).*/, "\\1 \\2 \\3 \\4 \\5 \\6", "g"))
-        if (last_time != "") {
-            time_diff = current_time - last_time
-            minutes = int(time_diff / 60)
-            seconds = time_diff % 60
-            printf "%s | Time since last drop: %02d:%02d\n", line, minutes, seconds
+        
+        if ($0 ~ /\| DROP$/) {
+            # Remove "| DROP " from the line
+            line = substr($0, 1, length($0) - 7)
+            if (last_drop_time != "") {
+                drop_diff = current_time - last_drop_time
+                drop_minutes = int(drop_diff / 60)
+                drop_seconds = drop_diff % 60
+                
+                outage_diff = current_time - last_success_time
+                outage_minutes = int(outage_diff / 60)
+                outage_seconds = outage_diff % 60
+                
+                printf "%s | Time since last drop: %02d:%02d | Outage duration: %02d:%02d\n", 
+                       line, drop_minutes, drop_seconds, outage_minutes, outage_seconds
+            } else {
+                print line " | First drop"
+            }
+            last_drop_time = current_time
         } else {
-            print line " | First drop"
+            last_success_time = current_time
         }
-        last_time = current_time
     }
 ' network_ping_log.txt > "$ERROR_REPORT"
